44c3bbb9fcc6aefc0f6dd5eac2110693b21e08e2
Krishan Pratap <krishan.pratap@bounteous.com>
2024-12-23 10:36:45 +0530

    Project

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,48 +1,4 @@
 
-// // src/App.js
-// import React from "react";
-// import Navbar from "./components/Navbar/Navbar";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import ApplePage from "./pages/Mobiles/ApplePage";
-// import SamsungPage from "./pages/Mobiles/SamsungPage";
-// import OnePlusPage from "./pages/Mobiles/OnePlusPage";
-// import DellPage from "./pages/Laptops/DellPage";
-// import HPPage from "./pages/Laptops/HPPage";
-// import LenovoPage from "./pages/Laptops/LenovoPage";
-// import SonyPage from "./pages/Televisions/SonyPage";
-// import LGPage from "./pages/Televisions/LGPage";
-// import SamsungTVPage from "./pages/Televisions/SamsungTVPage";
-// import CartPage from "./pages/CartPage/CartPage";
-// import { CartProvider } from "./contexts/CartContext";
-// import "./App.scss";
-
-// const App = () => {
-//   return (
-//     <CartProvider>
-//       <Router>
-//         <Navbar />
-//         <div className="page-container">
-//           <Routes>
-//             <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
-//             <Route path="/mobiles/apple" element={<ApplePage />} />
-//             <Route path="/mobiles/samsung" element={<SamsungPage />} />
-//             <Route path="/mobiles/oneplus" element={<OnePlusPage />} />
-//             <Route path="/laptops/dell" element={<DellPage />} />
-//             <Route path="/laptops/hp" element={<HPPage />} />
-//             <Route path="/laptops/lenovo" element={<LenovoPage />} />
-//             <Route path="/televisions/sony" element={<SonyPage />} />
-//             <Route path="/televisions/lg" element={<LGPage />} />
-//             <Route path="/televisions/samsung" element={<SamsungTVPage />} />
-//             <Route path="/cart" element={<CartPage />} />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </CartProvider>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -59,6 +15,7 @@ import CartPage from "./pages/CartPage/CartPage";
 import HomePage from "./pages/HomePage/HomePage";
 import { CartProvider } from "./contexts/CartContext";
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -78,8 +35,10 @@ const App = () => {
             <Route path="/televisions/lg" element={<LGPage />} />
             <Route path="/televisions/samsung" element={<SamsungTVPage />} />
             <Route path="/cart" element={<CartPage />} />
+
           </Routes>
         </div>
+        <Footer/>
       </Router>
     </CartProvider>
   );
---
+++ ecommerce-website/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Our Electronics Store. All rights reserved.</p>
+        <p>Follow us on:
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>,
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>,
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
